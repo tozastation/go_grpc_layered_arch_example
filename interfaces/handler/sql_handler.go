7499bf90e2ee5dc4d7266502e5a5bf2b7d4aa1e2
tozastation <b1016031@fun.ac.jp>
2019-03-05 12:49:26 +0900

    [Add] handler layer

---
+++ interfaces/handler/sql_handler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/denisenkom/go-mssqldb"
+	"os"
+)
+
+// OpenDBConnection is ...
+func OpenDBConnection() *sql.DB {
+	vender := os.Getenv("VENDER")
+	connectionString := os.Getenv("CONNECTION_STRING")
+	db, err := sql.Open(vender, connectionString)
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Failed to connected DB:", err)
+	}
+	fmt.Println("Establised DB Connection!!")
+	return db
+}
